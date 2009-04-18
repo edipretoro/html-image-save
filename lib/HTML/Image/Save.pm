3d14646ccb0e34e8130dee5864785e74d4c51794
Emmanuel Di Pretoro <edipretoro@gmail.com>
2009-04-18 22:34:36 +0200

    Change the $VERSION to 0.02

--- lib/HTML/Image/Save.pm
+++ lib/HTML/Image/Save.pm
@@ -27,11 +27,11 @@ HTML::Image::Save - Extract and save images from a HTML file
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 
 =head1 SYNOPSIS
